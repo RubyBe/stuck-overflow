988e06e447ef4abec6e0efbfd3dfea5cd6eaab96
RubyBe <sidbenavente@hotmail.com>
2016-09-02 07:58:12 -0700

    add show/hide an image

---
+++ app/components/answer-tile.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    },
+    delete(answer) {
+      if (confirm("Sure?")) {
+        this.sendAction('destroyAnswer', answer);
+      }
+    }
+  }
+});
