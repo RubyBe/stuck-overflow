dd44c0fe896f613d0b13e72968c956f22f878667
RubyBe <sidbenavente@hotmail.com>
2016-09-02 08:02:30 -0700

    add ability to destroy an answer

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,5 +4,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('answer');
+  },
+
+  actions: {
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('index');
+    }
   }
 });
